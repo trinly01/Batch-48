2536240c794155455dfd8b51d110e36bd27de6ae
Trinmar Boado <timi.boado@gmail.com>
2023-08-13 15:37:22 +0800

    Authenticated http requests using ofetch
     JWT Authorization headers

--- stores/cart.js
+++ stores/cart.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { ofetch } from 'ofetch';
 
 export const useCart = defineStore('cart', {
   state: () => ({
@@ -23,15 +24,47 @@ export const useCart = defineStore('cart', {
     async initialize () {
         console.log('i am a browser')
         // this.products = JSON.parse(localStorage.getItem('cart')) || []
-        this.products = await $fetch('http://localhost:3030/cart/user/123')
+        const jwt = localStorage.getItem('jwt') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkxOTA2MzAwLCJleHAiOjE2OTQ0OTgzMDB9.MnAUjYUMAufqMvlTrC6Poh2l2YtSeK47YJgI2c5tGqk'
+        const { data: cart } = await ofetch('http://localhost:1337/api/carts?filters[user]=1', {
+          headers: {
+            Authorization: "Bearer " + jwt,
+          }
+        })
+
+        this.products = cart.map(i => {
+          return {
+            id: i.id,
+            ...i.attributes
+          }
+        })
     },
     async addToCart (product) {
         this.products.push(product)
         localStorage.setItem('cart', JSON.stringify(this.products))
-        await $fetch('http://localhost:3030/cart/user/123', {
-          method: 'PUT',
+        // await $fetch('http://localhost:3030/cart/user/123', {
+        //   method: 'PUT',
+        //   body: {
+        //     cart: this.products
+        //   }
+        // })
+
+        const jwt = localStorage.getItem('jwt') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkxOTA2MzAwLCJleHAiOjE2OTQ0OTgzMDB9.MnAUjYUMAufqMvlTrC6Poh2l2YtSeK47YJgI2c5tGqk'
+        
+        await ofetch('http://localhost:1337/api/carts', {
+          headers: {
+            Authorization: "Bearer " + jwt,
+          },
+          method: 'POST',
           body: {
-            cart: this.products
+            data: {
+              user: 1,
+              title: product.title,
+              price: product.price,
+              description: product.description,
+              category: product.category,
+              image: product.image,
+              rating: product.rating
+            }
           }
         })
         console.log('client saved')
